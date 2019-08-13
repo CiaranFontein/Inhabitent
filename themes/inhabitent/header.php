23c914b73f867780b4e491684cf02f0a060dd0ab
Ciaran Fontein <ciaranfontein@gmail.com>
2019-08-13 15:27:52 -0700

    👀 Added about page and product post types 🐶🤹

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -33,6 +33,9 @@
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</nav><!-- #site-navigation -->
+					<div class="search-bar">
+						<?php get_search_form(); ?>
+					</div>
 				</div><!-- #container -->
 			</header><!-- #masthead -->
 
