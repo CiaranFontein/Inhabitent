b8653a1b2937ebacd933458eef9a667c50b22927
Ciaran Fontein <ciaranfontein@gmail.com>
2019-08-12 19:08:07 -0700

    💪Started flexing the css💪

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -21,15 +21,19 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+				<div class="container">
+					<div id="site-branding">
+						<div class='logo'>
+							<a href="https://http://localhost:8888/inhabitent/" rel="home">
+								<h1 class="site-title">Inhabitent</h1>
+							</a>
+						</div>
+					</div>
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</nav><!-- #site-navigation -->
+				</div><!-- #container -->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -17,6 +17,40 @@ function red_starter_body_classes( $classes ) {
 		$classes[] = 'group-blog';
 	}
 
+	if(is_singular('page')){
+		global $post;
+		$classes[] = 'page-' . $post->post_name;
+	}
+
 	return $classes;
 }
+
+function set_login_logo(){
+	echo '<style type="text/css">                                                                   
+	#login h1 a { background-image:url('.get_stylesheet_directory_uri().'/images/inhabitent-logo-text-dark.svg) !important; 
+	height: 53px !important; width: 300px !important; background-size: cover; }                            
+</style>';
+}
+
+add_action('login_head', 'set_login_logo');
+
+function set_login_logo_destination(){
+	return get_bloginfo('url');
+}
+add_filter('login_headerurl', 'set_login_logo_destination');
+
+function inhabitent_debug($var){
+	echo '<pre>';
+	var_dump($var);
+	echo '</pre>';
+}
+
+
+
 add_filter( 'body_class', 'red_starter_body_classes' );
+
+function inhabitent_remove_submenus() {
+    remove_submenu_page( 'themes.php', 'theme-editor.php' );
+    remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+}
+add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
