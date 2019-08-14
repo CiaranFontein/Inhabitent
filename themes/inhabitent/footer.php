8754b06ec6868585e9f501b7f7f5c339e5b61fed
Ciaran Fontein <ciaranfontein@gmail.com>
2019-08-13 19:23:52 -0700

    🙌footer style perfect and rem measured as much as possible😌

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,8 +10,46 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="footer-block-elements">
+					<div class="footer-block-element">
+						<div class="contact">
+							<h3>Contact Info</h3>
+							<p>
+								<i class="fa fa-envelope"></i>
+								<a href="mailto:ciaranfontein@gmail.com">info@inhabitent.com</a>
+							</p>
+							<p>
+								<i class="fa fa-phone"></i>
+								<a href="tel:5553434567891">778-456-7891</a>
+							</p>
+							<p>
+								<span><i class="fa fa-facebook-square"></i></span>
+								<span><i class="fa fa-twitter-square"></i></span>
+								<span><i class="fa fa-google-plus-square"></i></span>
+							</p>
+						</div><!-- contact -->
+					</div><!-- contact-info block element-->
+					<div class="footer-block-element">
+						<div class="business-hours">
+							<h3>Business Hours</h3>
+							<p><span class="day-of-week">Monday-Friday:</span>&nbsp 9am to 5pm</p>
+							<p><span class="day-of-week">Saturday:</span>&nbsp 10am to 2pm</p>
+							<p><span class="day-of-week">Sunday:</span>&nbsp Closed</p>
+						</div><!-- business-hours -->
+					</div><!-- block-element -->
+					<div class="footer-block-element">
+						<div class="text-logo">
+						<a href="http://localhost:3000/inhabitent/home" rel="home">
+							<img class="footer-logo-image" src="../wp-content/themes/inhabitent/images/inhabitent-logo-text.svg" alt="Inhabitent logo">
+						</a>
+					</div>
+				</div>
+				</div><!-- contact elements -->
+				
+				<div class="copyright-message">COPYRIGHT © 2019 INHABITENT</div>
+				</div>
+				
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
